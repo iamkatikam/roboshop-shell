204a98c2bcedbfae01fe9e461377f9b5cf76f2bd
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-24 19:02:57 +0530

    roboshop creating instances

---
+++ roboshop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+#INSTANCE_TYPE="t2.micro"
+#KEY_NAME="roboshop-key"
+SECURITY_GROUP="sg-0aba3fdb97c048851"
+#REGION="us-west-2"  
+ISTTANCES=("mongodb" "catalogue" "user" "payment" "shipping" "frontend" "rabbitmq" "redis" "mysql" "dispatch" "cart")
+ZONE_ID="Z0816687161ICZE30XIP3"
+DOMAIN_NAME="rameshaws.site"
+
+for instance in "${ISTTANCES[@]}"; do
+do
+  INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id ami-09c813fb71547fc4f \
+    --instance-type t2.micro \
+    --security-group-ids sg-0aba3fdb97c048851 \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" \
+	--query "Instances[0].InstanceId"
+	--output text)
+  if [ $instance != "frontend" ]; then
+    IP=$(aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query "Reservations[0].Instances[0].PrivateIpAddress" \
+    --output text)
+  else
+    IP=$(aws ec2 describe-instances \
+    --instance-ids $INSTANCE_ID \
+    --query "Reservations[0].Instances[0].PublicIpAddress" \
+    --output text)
+  fi
+  echo "Instance $instance created with ID: $INSTANCE_ID and IP: $IP"
+done
