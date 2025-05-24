9467bb9a2e653a4b395203322244000a298ec579
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-24 23:33:42 +0530

    roboshop creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -5,16 +5,32 @@ AMI_ID="ami-09c813fb71547fc4f"
 #KEY_NAME="roboshop-key"
 SECURITY_GROUP="sg-0aba3fdb97c048851"
 #REGION="us-west-2"  
-ISTTANCES=("mongodb" "catalogue" "user" "payment" "shipping" "frontend" "rabbitmq" "redis" "mysql" "dispatch" "cart")
+INSTANCES=("mongodb" "catalogue" "user" "payment" "shipping" "frontend" "rabbitmq" "redis" "mysql" "dispatch" "cart")
 ZONE_ID="Z0816687161ICZE30XIP3"
 DOMAIN_NAME="rameshaws.site"
 
-for instance in "${ISTTANCES[@]}"; do
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0aba3fdb97c048851 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
+for instance in "${INSTANCES[@]}"; do
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0aba3fdb97c048851 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
   else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
   fi
-  echo "Instance $instance created with ID: $INSTANCE_ID and IP: $IP"
+  echo "Instance $instance created and IP adress is: $IP"
+
+#Create Route 53 record
+    aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch '{
+      "Changes": [
+        {
+          "Action": "UPSERT",
+          "ResourceRecordSet": {
+            "Name": "'"$instance.$DOMAIN_NAME"'",
+            "Type": "A",
+            "TTL": 1,
+            "ResourceRecords": [{"Value": "'"$IP"'"}]
+          }
+        }
+      ]
+    }'
+    echo "Route 53 record for $instance.$DOMAIN_NAME created with IP: $IP"
 done
