8354950317c8636ed93c97da6165a7118e1828c0
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-25 17:46:57 +0530

    changes doen in roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,10 @@ INSTANCES=("mongodb" "catalogue" "user" "payment" "shipping" "frontend" "rabbitm
 ZONE_ID="Z0816687161ICZE30XIP3"
 DOMAIN_NAME="rameshaws.site"
 
+
 #for instance in "${INSTANCES[@]}"; do
 for instance in $@
+do
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0aba3fdb97c048851 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ $instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
